cbd2dca7596ac7b9cf781ee4a01bbfcb1e7ba905
YuliKors <82640538+YuliKors@users.noreply.github.com>
2021-04-25 19:49:00 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,20 +1,18 @@
-public class Main {
-    public static void main(String[] args){
-        HW7 hw7 = new HW7();
-        hw7.task1();
-        hw7.task2();
-        hw7.task3();
-        hw7.task5();
-        hw7.task6();
+class Main {
+    public static void main(String[] args) {
+        HW5 hw5 = new HW5();
+        HW5.DisplayOverloading task1 = new HW5.DisplayOverloading();
+        task1.disp(4);
+        task1.disp(4,5);
+        task1.disp(3.4, 4);
+        task1.disp(3,3.5);
+        task1.disp(10, 3.3f, "D");
+        hw5.task1();
+        hw5.task2(0,1, 0);
+        hw5.task3();
+        hw5.task4();
+        hw5.task5();
 
-        String first = "NY";
-        String second = "21";
-        String[] results = {"2NY1", "N21Y"};
-        for (String result : results) {
-            if (HW7.task4(first, second, result)) {
-                System.out.println(result + " is a valid shuffle of " + first + " and " + second);
-            } else {
-                System.out.println(result + " is not a valid shuffle of " + first + " and " + second);
-            }
-        }}
+    }
 }
+
