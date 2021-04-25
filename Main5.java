a8688e2938cd8fa643d0cb17ee4fc4749f7716e6
YuliKors <82640538+YuliKors@users.noreply.github.com>
2021-04-25 20:02:11 +0300

    Add files via upload

---
+++ Main5.java
@@ -0,0 +1,18 @@
+class Main5 {
+    public static void main(String[] args) {
+        HW5 hw5 = new HW5();
+        HW5.DisplayOverloading task1 = new HW5.DisplayOverloading();
+        task1.disp(4);
+        task1.disp(4,5);
+        task1.disp(3.4, 4);
+        task1.disp(3,3.5);
+        task1.disp(10, 3.3f, "D");
+        hw5.task1();
+        hw5.task2(0,1, 0);
+        hw5.task3();
+        hw5.task4();
+        hw5.task5();
+
+    }
+}
+
---
+++ Main6.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        HW6 hw6 = new HW6();
+        hw6.task1("Hello everyone!");
+        hw6.task2("The quick brown fox jumps");
+        hw6.task3("The quick brown fox jumps");
+        hw6.task4("The quick brown fox jumps");
+        hw6.task5();
+    }
+}
